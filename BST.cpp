5099bef007f361df5cb428b2d89756247ac6ada4
袁麓 <yuan@yuanludemacbook-pro.local>
2018-12-09 11:03:37 +0800

    binary search tree

--- BST.cpp
+++ BST.cpp
@@ -29,4 +29,34 @@ template <typename T> BinNodePosi(T) BST<T>::insert(const T &e){
 }
 
 //删除节点
-template <typename T> BinNodePosi(T) 
+template <typename T> bool BST<T>::remove(const T& e){
+    BinNodePosi(T) &x = search(e);
+    if(!x) return false;
+    removeAt(x, _hot);
+    this->_size--;
+    updateHeightAbove(_hot);
+    return true;
+}
+
+//BST节点删除算法（全局静态模版类函数，适用于AVL、Splay、RedBlack等各种BST）
+template <typename T>
+static BinNodePosi(T) removeAt(BinNodePosi(T) &x, BinNodePosi(T) &hot){
+    BinNodePosi(T) w = x;//实际被删除的节点，初值同x
+    BinNodePosi(T) succ = NULL;
+    if(!HasLChild(*x))
+        succ = x = x->rc;
+    else if (!HasRChild(*x))
+        succ = x = x->l;
+    else {
+        w = w->succ();
+        swap(x->data, w->data);
+        BinNodePosi(T) u = w->parent;
+        ((u == x) ? u->rc : u->lc) = succ = w->rc;
+    }
+    hot = w->parent;
+    if (succ) succ->parent = hot;
+        release(w->data);
+        release(w);
+        return succ;
+}
+
